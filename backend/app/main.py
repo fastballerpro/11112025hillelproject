168dbb8d18a673452edee711dd44e1b84664e23f
John Appleseed <john@gmail.com>
2026-02-03 19:36:55 +0200

    env

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from apps.users.routers import users_router
+from settings import settings
 
 
 def get_application() -> FastAPI:
-    _app = FastAPI()
+    _app = FastAPI(
+        debug=settings.DEBUG
+    )
 
     _app.include_router(users_router, prefix="/users", tags=["users"])
 
