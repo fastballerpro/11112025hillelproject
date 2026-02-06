6e509655cc0c4b064ef0a1e294dc038fec322367
John Appleseed <john@gmail.com>
2026-02-06 20:13:52 +0200

    migrations init

---
+++ backend/app/apps/users/models.py
@@ -0,0 +1,9 @@
+from apps.core.base_model import Base
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+class User(Base):
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str]
+    
